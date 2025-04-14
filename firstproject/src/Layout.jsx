088313ae830d37e886e1873784d1ed8202edefbb
nikhil0441 <nikhilpatel03022004@gmail.com>
2025-04-14 18:23:43 +0530

    commnet

--- firstproject/src/Layout.jsx
+++ firstproject/src/Layout.jsx
@@ -1,11 +1,12 @@
 
 import { Outlet } from "react-router-dom";
+import Footer from "./Components/Footer";
 const Layout = () => {
   return (
     <>
        
       <Outlet/>
-      
+      <Footer/>      
     </>
   )
 };
--- firstproject/src/Pages/Servies.jsx
+++ firstproject/src/Pages/Servies.jsx
@@ -1,5 +1,44 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
 const Service = () => {
-  return <div>Servies Page</div>;
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
 };
 
 export default Service;
