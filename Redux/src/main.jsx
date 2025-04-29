c656e9f798d71272fa7f5944df4d96bf6eb19998
nikhil0441 <nikhilpatel03022004@gmail.com>
2025-04-29 17:46:36 +0530

    commnet

--- Redux/src/main.jsx
+++ Redux/src/main.jsx
@@ -1,11 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-import store from "./store.jsx"; // import store from the store file
-import { Provider } from "react-redux"; // import Provider from react-redux
-
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  
     <App />
-  </Provider>
+  
 );
